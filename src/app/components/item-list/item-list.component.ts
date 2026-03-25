82a3112c2f7ef6c68035fecc9f49bc6e8de5a7b3
arulkg17 <arulkg@yahoo.com>
2026-03-25 15:20:17 -0500

    added pagination

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from "@angular/core";
+import { Component, OnInit, ViewChild, signal } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { ItemmasterService } from "../../services/itemmaster.service";
@@ -9,92 +9,131 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 import { ConfirmDialogComponent } from "../../shared/confirm-dialog/confirm-dialog.component";
 import { ItemFormComponent } from "../item-form/item-form.component";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
-    selector:'app-item-list',
-    standalone:true,
-    imports:[CommonModule,
-        RouterModule,
-        MatTableModule,
-        MatButtonModule,
-        MatDialogModule],
-    templateUrl:'./item-list.component.html',
-    styleUrls:['./item-list.component.css']
+  selector: 'app-item-list',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatTableModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent implements OnInit {
-    items = signal<Itemmaster[]>([]);
-        // Add a MatTableDataSource to connect with mat-table
-    dataSource!: MatTableDataSource<Itemmaster>;
-    displayedColumns:string[] = 
-            [
-                'catCode','itemBarCode', 'itemCode','itemName',
-                'description','uom','rate','minimumStock','maximumStock',
-                'isActive','actions'
-            ];
-    constructor(
-        private service:ItemmasterService,
-        private snackBar:MatSnackBar,
-        private dialog:MatDialog
-    ){}
-    ngOnInit(): void {
-        this.loadItems();
-    }
+  items = signal<Itemmaster[]>([]);
+  dataSource!: MatTableDataSource<Itemmaster>;
+  displayedColumns: string[] = [
+    'catCode','itemBarCode','itemCode','itemName',
+    'description','uom','rate','minimumStock','maximumStock','isActive','actions'
+  ];
 
-    loadItems(){
-        return this.service.getAll().subscribe({
-            next: (res:any) =>{ 
-                this.items.set(res.data);
-                this.dataSource = new MatTableDataSource(res.data);
-            },
-           error:()=>{
-               this.snackBar.open(
-                'Item Loading Error',
-                'Close',
-                {duration:3000});
-           }
+  isLoading = false;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private service: ItemmasterService,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    this.loadItems();
+  }
+
+  loadItems() {
+    this.isLoading = true;
+    this.service.getAll().subscribe({
+      next: (res: any) => {
+        this.items.set(res.data);
+        this.dataSource = new MatTableDataSource(res.data);
+
+        // Assign paginator & sort
+        setTimeout(() => {
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
         });
+
+        // Global filter across all columns
+        this.dataSource.filterPredicate = (data: Itemmaster, filter: string) => {
+          const dataStr = Object.values(data).join(' ').toLowerCase();
+          return dataStr.includes(filter);
+        };
+
+        this.isLoading = false;
+      },
+      error: () => {
+        this.isLoading = false;
+        this.snackBar.open('Item Loading Error', 'Close', { duration: 3000 });
+      }
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    if (this.dataSource) {
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+      if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+      }
     }
-    delete(id:number, name:string){
-        const dialogRef = this.dialog.open(ConfirmDialogComponent, {
-                width: '350px',
-                data: { name }
-        });
-        dialogRef.afterClosed().subscribe(result => {
-            if(!result) return;
-            this.service.delete(id).subscribe({
-                next: () =>{
-                    //this.items.update(list=>list.filter(i=>i.id !== id));
-                    //this.dataSource.data = this.items();
-                    this.loadItems();
-                    this.snackBar.open('Item delete successfully','Close',
-                        {duration:3000}
-                    );
-                },
-                error: () =>{
-                    this.snackBar.open('Item delete error', 'Close',
-                        {duration:3000});
-                }
-            });
-        });
-    } 
-    openAddDialog(){
-        const dialogRef = this.dialog.open(ItemFormComponent, {
-            width:'700px',
-            maxHeight:'80vh',
-            panelClass:'custom-dialog',
-            data:null
-        });
-        dialogRef.afterClosed().subscribe(result=>{
-            if(result) this.loadItems();
-        });
-    }
-    openEditDialog(item:any){
-        const dialogRef = this.dialog.open(ItemFormComponent, {
-            width:'700px',
-            data:item
-        });
-        dialogRef.afterClosed().subscribe(result=>{
-            if(result) this.loadItems();
-        });
-    }
+  }
+
+  delete(id: number, name: string) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '350px',
+      data: { name }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (!result) return;
+      this.service.delete(id).subscribe({
+        next: () => {
+          this.snackBar.open('Item deleted successfully', 'Close', { duration: 3000 });
+          this.loadItems();
+        },
+        error: () => {
+          this.snackBar.open('Item delete error', 'Close', { duration: 3000 });
+        }
+      });
+    });
+  }
+
+  openAddDialog() {
+    const dialogRef = this.dialog.open(ItemFormComponent, {
+      width: '900px',
+      maxHeight: '90vh',
+      panelClass: 'custom-dialog',
+      autoFocus: false,
+      data: null
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) this.loadItems();
+    });
+  }
+
+  openEditDialog(item: any) {
+    const dialogRef = this.dialog.open(ItemFormComponent, {
+      width: '900px',
+      maxHeight: '90vh',
+      panelClass: 'custom-dialog',
+      autoFocus: false,
+      data: item
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) this.loadItems();
+    });
+  }
 }
